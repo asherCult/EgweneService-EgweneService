cfef345e6dbc27e623215b1eff5f4a139b5ea4e3
asherCult-dev <asherCult-dev@tra.sk>
2023-03-12 10:54:55 -0400

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace EgweneService.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
